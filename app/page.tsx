c144e38ceb73e3963eb4365273ebd6720679d381
miftah <miftahulnbl56@gmail.com>
2026-06-18 22:34:15 +0700

    feat: initial Nextjs setup with Tailwind

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="main-h-screen p-8">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Halo, web berita siap dikembangkan dengan nextjs!
+      </h1>
+    </main>
+  );
+}
